66bf3441234d259d03c14cea7a19a8ae20955392
Chaambane <72652379+Chaambane@users.noreply.github.com>
2021-03-01 18:45:10 +0100

    refact(middleware) delete

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
-const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
-const app = express();
 
 
 mongoose.connect('mongodb+srv://chaambane:So_Pekocko@cluster0.pmz2j.mongodb.net/chaambane?retryWrites=true&w=majority',
@@ -16,11 +16,14 @@ mongoose.connect('mongodb+srv://chaambane:So_Pekocko@cluster0.pmz2j.mongodb.net/
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+const app = express();
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
@@ -28,7 +31,9 @@ app.use((req, res, next) => {
 app.use(bodyParser.json()); // on parse les données reçue pour pouvoir les éxploités. 
 
 
-app.use('api/sauce', sauceRoutes); // Passage de toutes les requêtes.
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- models/sauce.js
+++ models/sauce.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+
+
 const sauceSchema = mongoose.Schema({
     userId: {type: String, required: true},
     name: {type: String, required: true},
